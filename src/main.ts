236bdc41f8f38bc2f3f8eccc42094d089c938653
Aadith <2004aadith@gmail.com>
2023-07-13 19:31:16 +0530

    nest commit

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.use('/uploads',Express.static(join(__dirname, '..', 'uploads')));
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
